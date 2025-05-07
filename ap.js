791ea1d2a115dbc126807a88cc07cf19fa260b2e
AzizullahCodes <azizullahcodes@gmail.com>
2025-05-07 21:59:06 +0500

    Initial commit

---
+++ ap.js
@@ -0,0 +1,16 @@
+var bookTitle = "The Life of Allama Iqbal";
+var authorName = "Javaid Iqbal";
+var totalPages = 200;
+var price = "500 Rupees";
+var publisherName = "Azeem and Sons Publishers";
+alert(" Book Title : " + bookTitle + "\n" + " Author Name : " + authorName + "\n" + " Total Pages : " + totalPages + "\n" + " Price : " + price + "\n" + " Publisher : " + publisherName);
+
+// project-02
+
+var breakfastCost = 200;
+var lunchCost = 250;
+var dinnerCost = 300;
+var transportCost = 200 ;
+var totalExpenses = breakfastCost + lunchCost + dinnerCost + transportCost + transportCost ;
+console.log(totalExpenses);
+alert(" Breakfast : " + breakfastCost + "\n" + " Lunch : " + lunchCost + "\n" + " Dinner : " + dinnerCost + "\n" + " Transport : " + transportCost + "\n" + "------------------" + "\n" + " Total Expense : " + totalExpenses + " PKR ");
